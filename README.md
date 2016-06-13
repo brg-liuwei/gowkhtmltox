a954763cf212231441cb4a670edfae6d81a26c5d
brg-liuwei <stupidlw@126.com>
2016-06-13 20:51:56 +0800

    demo service

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ gowkhtmltox is a golang wrapper for [wkhtmltopdf](https://github.com/wkhtmltopdf
 ### Demo
 
     go get -u github.com/brg-liuwei/gowkhtmltox/gowkhtmltox-demo
+
+    # run service
+    gowkhtmltox-demo -port 9999
+
+    # test (http GET)
+    curl "http://127.0.0.1:9999/render/url?url=https://www.baidu.com"
+
+    # test (http POST)
+    curl http://127.0.0.1:9999/render/html -d '<html><body><h1>Hello World</h1></body></html>'
--- gowkhtmltox-demo/gowkhtmltox-demo.go
+++ gowkhtmltox-demo/gowkhtmltox-demo.go
@@ -1,34 +1,87 @@
 package main
 
 import (
-	"encoding/binary"
+	"flag"
 	"fmt"
-	"os"
-	"time"
+	"io/ioutil"
+	"net/http"
 
 	img "github.com/brg-liuwei/gowkhtmltox/gowkhtmltoimage"
 )
 
+var port = flag.Int("port", 9999, "http listen port")
+
+func urlRender(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "", http.StatusForbidden)
+		return
+	}
+	url := r.FormValue("url")
+	if len(url) == 0 {
+		http.Error(w, "need get param: url=urlToRender", http.StatusOK)
+		fmt.Println("no param")
+		return
+	}
+	conv := img.NewConvertor()
+	conv.SetProperty("web.loadImages", "true")
+	conv.SetProperty("fmt", "jpg")
+	conv.SetProperty("in", "url")
+	if err := conv.Ready(); err != nil {
+		http.Error(w, "render ready error", http.StatusOK)
+		fmt.Println("url render ready error")
+		return
+	}
+	if err := conv.Run(); err != nil {
+		http.Error(w, "render error", http.StatusOK)
+		fmt.Println("url render run error")
+		return
+	}
+	w.Header().Set("Content-Type", "image/jpg")
+	w.Write(conv.GetImage())
+}
+
+func htmlRender(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "", http.StatusForbidden)
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "read body error: "+err.Error(), http.StatusOK)
+		fmt.Println("html render read body error")
+		return
+	}
+	conv := img.NewConvertor()
+	conv.SetProperty("web.loadImages", "true")
+	conv.SetProperty("fmt", "jpg")
+	conv.SetHtml(string(body))
+	if err := conv.Ready(); err != nil {
+		http.Error(w, "render ready error", http.StatusOK)
+		fmt.Println("html render ready error")
+		return
+	}
+	if err := conv.Run(); err != nil {
+		http.Error(w, "render error", http.StatusOK)
+		fmt.Println("html render run error")
+		return
+	}
+	w.Header().Set("Content-Type", "image/jpg")
+	w.Write(conv.GetImage())
+}
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
 	img.Init(false)
 
-	conv := img.NewConvertor()
-	conv.SetProperty("web.loadImages", "true")
-	conv.SetProperty("fmt", "jpeg")
-	// conv.SetProperty("in", "https://www.baidu.com/")
-	conv.SetHtml("<html><body><h1>Hello World</h1></body></html>")
+	http.HandleFunc("/render/url", urlRender)
+	http.HandleFunc("/render/html", htmlRender)
 
-	if err := conv.Ready(); err != nil {
-		panic("ready error")
-	}
+	fmt.Println("port = ", *port)
 
-	now := time.Now()
-	if err := conv.Run(); err != nil {
-		panic("run error")
-	}
-	fmt.Fprintln(os.Stderr, "use time: ", time.Since(now))
-
-	binary.Write(os.Stdout, binary.LittleEndian, conv.GetImage())
+	panic(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 
 	img.DeInit()
 }
